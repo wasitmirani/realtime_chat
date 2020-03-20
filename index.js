5800fe11666482d81f492af850ac10b2c1fd8d86
Wasit Mirani <44178287+wasitmirani@users.noreply.github.com>
2020-03-20 23:32:29 +0500

    new

--- index.js
+++ index.js
@@ -1,65 +1,59 @@
-const app = require('express')()
-const http = require('http').createServer(app)
-
-
-app.get('/', (req, res) => {
-    res.send("Node Server is running. Socket Io Testing !!")
-})
-
-//Socket Logic
-const socketio = require('socket.io')(http)
-
-socketio.on("connection", (userSocket) => {
-    userSocket.on("send_message", (data) => {
-        userSocket.broadcast.emit("receive_message", data)
-    })
-})
-
-http.listen(process.env.PORT)
-
-// let app = require('express')();
-
-// let http = require('http').createServer(app);
-
-// let io = require('socket.io')(http);
+// const app = require('express')()
+// const http = require('http').createServer(app)
 
 // app.get('/', (req, res) => {
-//     res.sendFile(__dirname + '/index.html');
+//     res.send("Node Server is running. Socket Io Testing !!")
 // })
 
-// io.on('connection', (socket) => {
-//     io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+// //Socket Logic
+// const socketio = require('socket.io')(http)
 
-
-//     socket.on('disconnect', () => {
-//         console.log('disconnected')
-//         io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+// socketio.on("connection", (userSocket) => {
+//     userSocket.on("send_message", (data) => {
+//         userSocket.broadcast.emit("receive_message", data)
 //     })
-
-
-
-//     socket.on('chat-message', (msg) => {
-//         socket.broadcast.emit('chat-message', msg)
-//     })
-//     socket.on('joined', (name) => {
-//         socket.broadcast.emit('joined', name)
-//     })
-//     socket.on('leaved', (name) => {
-//         socket.broadcast.emit('leaved', name)
-//     })
-
-//     socket.on('typing', (data) => {
-//         socket.broadcast.emit('typing', data)
-//     })
-//     socket.on('stoptyping', () => {
-//         socket.broadcast.emit('stoptyping')
-//     })
-
-
 // })
+
+// http.listen(process.env.PORT)
+
+let app = require("express")();
+
+let http = require("http").createServer(app);
+
+let io = require("socket.io")(http);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", socket => {
+  io.emit("noOfConnections", Object.keys(io.sockets.connected).length);
+
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+    io.emit("noOfConnections", Object.keys(io.sockets.connected).length);
+  });
+
+  socket.on("chat-message", msg => {
+    socket.broadcast.emit("chat-message", msg);
+  });
+  socket.on("joined", name => {
+    socket.broadcast.emit("joined", name);
+  });
+  socket.on("leaved", name => {
+    socket.broadcast.emit("leaved", name);
+  });
+
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
+  });
+  socket.on("stoptyping", () => {
+    socket.broadcast.emit("stoptyping");
+  });
+});
 
 // http.listen(80, () => {
-//     console.log('Server is started at http://localhost:3000')
-// })
+//   console.log("Server is started at http://localhost:3000");
+// });
 
-// // http.listen(process.env.PORT)
+http.listen(process.env.PORT);
